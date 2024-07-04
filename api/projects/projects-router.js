e6d8d2d60837e48018ae23088636a9fed070973d
DerekG29 <derekgrubbs29@gmail.com>
2024-07-04 10:35:10 -0400

    can successfully GET to /api/projects

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,9 +1,35 @@
 const express = require('express');
+const Projects = require('./projects-model');
+const { checkId } = require('./projects-middleware');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.status(200).json({ message: 'hello from projects!' });
-})
+router.get('/', (req, res, next) => {
+  Projects.get()
+    .then(projects => {
+      res.status(200).json(projects);
+    })
+    .catch(next);
+});
+
+router.get('/:id', checkId, (req, res) => {
+  
+});
+
+router.post('/', (req, res) => {
+
+});
+
+router.put('/:id', checkId, (req, res) => {
+
+});
+
+router.delete('/:id', checkId, (req, res) => {
+
+});
+
+router.get('/:id/actions', checkId, (req, res) => {
+
+});
 
 module.exports = router;
