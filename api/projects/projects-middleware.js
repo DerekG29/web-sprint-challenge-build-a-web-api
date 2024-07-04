c445f320f3bae3fc8074f9bd59ebf01e9479bfb6
DerekG29 <derekgrubbs29@gmail.com>
2024-07-04 11:39:19 -0400

    adjusted logic in checkPost middleware

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -21,7 +21,7 @@ function checkId(req, res, next) {
 
 function checkPost(req, res, next) {
   const { name, description, completed } = req.body;
-  if (!name || !description || (req.method === 'PUT' || !completed)) {
+  if (!name || !description || (req.method === 'PUT' ? !completed : false)) {
     res.status(400).json({ message: 'a name and description are required' });
   } else {
     next();
