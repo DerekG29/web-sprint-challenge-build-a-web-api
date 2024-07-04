1f5ea0e07701ff716ae98cb0544a224ea1422581
DerekG29 <derekgrubbs29@gmail.com>
2024-07-04 10:34:02 -0400

    built middleware to check ids

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,19 @@
-// add middlewares here related to projects
+const Projects = require('./projects-model');
+
+module.exports = {
+  checkId,
+}
+
+function checkId(req, res, next) {
+  const id = req.params.id;
+  Projects.get(id)
+    .then(project => {
+      if (!project) {
+        res.status(404).json({ message: `Project with ID ${id} not found` });
+      } else {
+        req.project = project;
+        next();
+      }
+    })
+    .catch(next);
+}
