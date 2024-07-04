fa7c0f951159c22466b7ad5d48b721de7da96e3a
DerekG29 <derekgrubbs29@gmail.com>
2024-07-04 10:12:44 -0400

    made a logger middleware

--- api/server.js
+++ api/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const actionsRouter = require('./actions/actions-router');
+const projectsRouter = require('./projects/projects-router');
 const server = express();
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
-
 server.use(express.json());
+
+server.use((req, res, next) => {
+  console.log(
+    `[${new Date().toISOString()}] ${req.method} to ${req.url}`
+  )
+  next();
+})
+
+// server.use('/api/actions', actionsRouter);
+// server.use('/api/projects', projectsRouter);
 
 server.get('/', (req, res) => {
   res.status(200).json({ message: 'Hello World' });
