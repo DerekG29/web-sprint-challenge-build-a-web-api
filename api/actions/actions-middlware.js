11dde1d2a2a9b8f4263b8f735536d498358fc106
DerekG29 <derekgrubbs29@gmail.com>
2024-07-05 13:41:31 -0400

    built checkPost_actions middleware

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,10 +1,12 @@
 const Actions = require('./actions-model');
+const Projects = require('../projects/projects-model')
 
 module.exports = {
-  checkId,
+  checkId_actions,
+  checkPost_actions,
 }
 
-function checkId(req, res, next) {
+function checkId_actions(req, res, next) {
   const id = req.params.id;
   Actions.get(id)
     .then(action => {
@@ -16,4 +18,26 @@ function checkId(req, res, next) {
       }
     })
     .catch(next);
+}
+
+async function checkPost_actions(req, res, next) {
+  try {
+    const { project_id, description, notes } = req.body;
+    if (!project_id || !description || !notes) {
+      res.status(400).json({
+        message: 'project_id, description, and notes are required'
+      });
+    } else {
+      const project = await Projects.get(project_id);
+      if (!project) {
+        res.status(404).json({
+          message: `Project with id ${project_id} not found`
+        });
+      } else {
+        next();
+      }
+    }
+  } catch (error) {
+    next(error);
+  }
 }
