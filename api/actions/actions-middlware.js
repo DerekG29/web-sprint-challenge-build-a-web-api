fdd69e01483f9c4a80d7d33fb9af547e39fb5bc0
DerekG29 <derekgrubbs29@gmail.com>
2024-07-05 13:19:47 -0400

    built checkId middleware for actions

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,19 @@
-// add middlewares here related to actions
+const Actions = require('./actions-model');
+
+module.exports = {
+  checkId,
+}
+
+function checkId(req, res, next) {
+  const id = req.params.id;
+  Actions.get(id)
+    .then(action => {
+      if (!action) {
+        res.status(404).json({ message: `No actions found with id ${id}`})
+      } else {
+        req.action = action;
+        next();
+      }
+    })
+    .catch(next);
+}
