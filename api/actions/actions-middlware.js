bc72b446ce76786571c96774aaa386cbe5c2d96f
DerekG29 <derekgrubbs29@gmail.com>
2024-07-05 14:01:47 -0400

    built checkPut_actions middleware

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -4,6 +4,7 @@ const Projects = require('../projects/projects-model')
 module.exports = {
   checkId_actions,
   checkPost_actions,
+  checkPut_actions
 }
 
 function checkId_actions(req, res, next) {
@@ -33,6 +34,34 @@ async function checkPost_actions(req, res, next) {
         res.status(404).json({
           message: `Project with id ${project_id} not found`
         });
+      } else {
+        next();
+      }
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function checkPut_actions(req, res, next) {
+  try {
+    const { project_id, description, notes } = req.body;
+    const completed = req.body.completed !== undefined;
+    if (
+      !project_id ||
+      !description ||
+      !notes ||
+      !completed
+    ) {
+      res.status(400).json({
+        message: 'project_id, description, notes, and completed are required'
+      })
+    } else {
+      const project = await Projects.get(project_id);
+      if (!project) {
+        res.status(400).json({
+          message: `Project with id ${project_id} not found`
+        })
       } else {
         next();
       }
